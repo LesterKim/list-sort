f068c751d2124cd265c6490bef7d8bb3e5933e6a
Lester Kim <lester.c.kim@gmail.com>
2020-08-09 09:46:35 -0400

    fix(list_sorter.py): Fixed import and removed assertions

--- sort/list_sorter.py
+++ sort/list_sorter.py
@@ -1,5 +1,5 @@
 from sort.numbers import Numbers
-from sort.runtime_printer import runtime_printer
+from sort.utilities.runtime_printer import runtime_printer
 from sort.utilities.sort import sort
 
 
@@ -10,12 +10,3 @@ class ListSorter:
   @runtime_printer
   def sort(self) -> Numbers:
     return sort(self._numbers, 'mergesort')
-
-sorted = runtime_printer(sorted)
-
-numbers = [8, -9, 0, 222, 81, 53, 45, 99, -88, 344, 146, -958, 222, 353, 0, -2, 222, 0, 0, 8, -9, 0, 222, 81, 53, 45, 99, -88, 344, 146, -958, 222, 353, 0, -2, 222, 0, 0, -958, 222, 353, 0, -2, 222, 0, 0, 8, -9, 0, 222, 81, 53, 45, 99, -88]
-assert ListSorter(numbers).sort() == sorted(numbers)
-
-# n = 2 ** 19
-# numbers = list(range(n - 1, 0, -1))
-# assert ListSorter(numbers).sort() == list(range(1, n))
